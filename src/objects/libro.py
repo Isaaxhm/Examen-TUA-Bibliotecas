b889e5f9fd44ffbfbaa298203b2b70072e2bbbd6
Isaac Herrera <isaac.herrera@tua.edu.mx>
2026-07-02 11:18:19 -0600

    Estructura para el libro

---
+++ src/objects/libro.py
@@ -0,0 +1,16 @@
+"""_summary_
+
+Un objeto, Libro, con los atributos de:
+- id, 
+- titulo, 
+- autor, 
+- categoria, 
+- disponible.
+
+Metodos
+- Agregar libros al catálogo.
+- Mostrar todos los libros registrados.
+- Buscar un libro específico por su título.
+- Cambiar el estado de disponibilidad de un libro.
+
+"""
